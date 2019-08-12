ba757ee87e02fd2dcb7f5da0258a4698e57dcf16
Mendez324 <48069230+Mendez324@users.noreply.github.com>
2019-08-12 16:28:16 +0100

    added small burguer that needs style

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
 	$('#wrapper').fullpage({
 		//options here
-		
 		navigation: true
 	});
-
 	//methods
 	$.fn.fullpage.setAllowScrolling(true);
+
+
+
+	$('.burger-menu').click(function(){
+		$(this).toggleClass('burger-menu--open');
+	});
 });
